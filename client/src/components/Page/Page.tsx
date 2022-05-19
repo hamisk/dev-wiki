1ae7af90edfbb89ac257439d7d0c452990936409
hamisk <hs.khera@icloud.com>
2022-05-18 22:32:52 -0700

    Add functionality to edit section markdown

--- client/src/components/Page/Page.tsx
+++ client/src/components/Page/Page.tsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Section from '../Section/Section';
+import './Page.scss';
 const apiURL = 'http://localhost:4000';
 
 type Props = {
@@ -9,21 +11,34 @@ type Props = {
 
 function Page({ user }: Props) {
   const [page, setPage] = useState<any>([]);
-  const [sections, setSections] = useState([]);
+  const [sections, setSections] = useState<any>([]);
   const params = useParams();
 
   useEffect(() => {
     axios.get(apiURL + '/page/' + params.title).then(res => {
       setPage(res.data.page);
-      setSections(res.data.page.contents.sections);
+      setSections(res.data.page.sections);
+      console.log(res.data.page);
     });
   }, [params]);
 
   const addSection = () => {};
 
   return (
-    <div>
+    <div className='page'>
       <h1 className='page__title'>{page.pageTitle || 'loading'}</h1>
+      {sections
+        ? sections.map((section: any) => {
+            return (
+              <Section
+                section={section}
+                key={section.sectionId}
+                user={user}
+                path={params.id + '/sections/' + section.sectionId}
+              />
+            );
+          })
+        : ''}
       {user ? (
         <button className='page__button' onClick={addSection}>
           Add Section
--- client/src/components/Section/Section.tsx
+++ client/src/components/Section/Section.tsx
@@ -1,10 +1,48 @@
-import React from 'react';
-import reactMarkdown from 'react-markdown';
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import './Section.scss';
 
-type Props = {};
+type Props = {
+  section: any;
+  user: any;
+  path: any;
+};
 
-function Section({}: Props) {
-  return <div>Section</div>;
+function Section({ section, user, path }: Props) {
+  const [content, setContent] = useState(section.content);
+  const [editing, setEditing] = useState<boolean>(user && user.username === section.editor);
+  // editing = boolean: user - to see if a user is currently logged in
+  // user.username === section.editor
+  // new sections have an editor assigned when created - the current user who created it
+  // when user clicks on a section, they become editor for that section, so that they can edit it
+  // editing - tells us whether or not the current user is editing
+  // we use this to decide whether content is rendered into html, or as markdown in a textbox to be edited
+
+  const startEditing = (e: any) => {
+    // don't run this function if we don't have a logged in user, or if they're already editing
+    if (!user || editing) return;
+
+    setEditing(true);
+
+    // also need to store in the database that the current user is now the editor of this section
+    // to let other users know that it's currently being edited, so they can't edit it
+    console.log(path);
+  };
+  // console.log(user.username === section.editor);
+  console.log(user);
+  // console.log(section.editor);
+
+  console.log(editing);
+
+  return (
+    <section className='section' onClick={startEditing}>
+      {editing ? (
+        <textarea className='section__editing' defaultValue={content} />
+      ) : (
+        <ReactMarkdown>{content}</ReactMarkdown>
+      )}
+    </section>
+  );
 }
 
 export default Section;
