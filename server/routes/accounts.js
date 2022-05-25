94bdf2c2ccddaf3b9b82dc369879627a5221af54
hamisk <hs.khera@icloud.com>
2022-05-24 21:21:54 -0700

    Add jwt to signup route

--- server/routes/accounts.js
+++ server/routes/accounts.js
@@ -41,8 +41,18 @@ router.post('/signup', (req, res) => {
 
   users.push(userObject);
   fs.writeFileSync('./database/users.json', JSON.stringify(users));
-  req.session.user = userObject;
-  res.json({ signedIn: true, user: userObject });
+  // create and return JWT
+  const token = jwt.sign(
+    { username: username }, // 1. payload
+    process.env.JWT_SECRET_KEY, // 2. secret key
+    { expiresIn: '6h' } // 3. options
+  );
+
+  res.status(200).json({
+    signedIn: true,
+    message: 'Successfully logged in',
+    token: token,
+  });
 });
 
 router.post('/signin', (req, res) => {
@@ -59,15 +69,11 @@ router.post('/signin', (req, res) => {
     if (user.passwordHash !== hash(password)) {
       return res.json({ signedIn: false, message: 'wrong username or password' });
     }
-    // it is a valid password at this point,
-    // create and return JWT
+    // it is a valid password at this point, create and return JWT
     const token = jwt.sign(
-      // 1. payload
-      { username: username },
-      // 2. secret key
-      process.env.JWT_SECRET_KEY,
-      // 3. options
-      { expiresIn: '6h' }
+      { username: username }, // 1. payload
+      process.env.JWT_SECRET_KEY, // 2. secret key
+      { expiresIn: '6h' } // 3. options
     );
 
     res.status(200).json({
@@ -88,108 +94,3 @@ router.post('/signout', (req, res) => {
 });
 
 module.exports = router;
-
-// const bcrypt = require('bcrypt');
-// const salt = 10;
-
-// exports.createUser = (req, res) => {
-//   // const name = req.body.name;
-//   // const username = req.body.username;
-//   // const password = req.body.password;
-//   const { name, username, password, carbon, city, country } = req.body;
-
-//   if (!name || !username || !password || !carbon) {
-//     return res.status(400).json({
-//       message: 'Register requires name, username, and password',
-//     });
-//   }
-
-//   // at this point, we are guaranteed to have a
-//   // name, username, and password
-
-//   bcrypt.hash(password, salt, function (err, hashedPassword) {
-//     const newUser = {
-//       name: name,
-//       username: username,
-//       password: hashedPassword,
-//       city: city,
-//       country: country,
-//       goal_carbon: carbon,
-//     };
-
-//     console.log(newUser);
-
-//     knex('users')
-//       .insert(newUser)
-//       .then(() => {
-//         // create and return JWT
-//         const token = jwt.sign(
-//           // 1. payload
-//           { username: username },
-//           // 2. secret key
-//           process.env.JWT_SECRET_KEY,
-//           // 3. options
-//           { expiresIn: '6h' }
-//         );
-
-//         res.status(200).json({
-//           message: 'Successfully registered',
-//           token: token,
-//         });
-//       })
-//       .catch(err => res.status(400).send(`Error registering: ${err}`));
-//   });
-// };
-
-// exports.login = (req, res) => {
-//   const { username, password } = req.body;
-
-//   if (!username || !password) {
-//     return res.status(400).json({
-//       message: 'Login requires username and password fields',
-//     });
-//   }
-
-//   // username and password are provided
-//   knex('users')
-//     .where({ username: username })
-//     .then(user => {
-//       const foundUser = user[0];
-//       // console.log(user)
-//       // console.log(foundUser)
-
-//       if (!foundUser) {
-//         return res.status(400).json({
-//           message: 'User does not exist',
-//         });
-//       }
-
-//       // we are guaranteed to have the user here
-//       // Validate password matches user's password
-//       bcrypt.compare(password, foundUser.password, (err, result) => {
-//         if (!result) {
-//           // invalid password, return response
-//           return res.status(403).json({
-//             message: 'Invalid Credentials, password does not match',
-//           });
-//         }
-
-//         // it is a valid password at this point,
-//         // create and return JWT
-//         const token = jwt.sign(
-//           // 1. payload
-//           { username: username },
-//           // 2. secret key
-//           process.env.JWT_SECRET_KEY,
-//           // 3. options
-//           { expiresIn: '6h' }
-//         );
-
-//         res.status(200).json({
-//           message: 'Successfully logged in',
-//           token: token,
-//         });
-//       });
-//     })
-//     .catch(err => res.status(400).send(`Error retrieving user: ${err}`));
-// };
