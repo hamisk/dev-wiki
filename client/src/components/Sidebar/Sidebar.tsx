3cd6f41156f9e8b658210e83ba68f2daf4ed3c58
hamisk <hs.khera@icloud.com>
2022-05-27 20:26:26 -0700

    Add logo to sidebar, favicon

--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Login from '../Login/Login';
 import PageList from '../PageList/PageList';
 import './SideBar.scss';
+import dwLogo from '../../assets/logo/dw-wiki.png';
 
 type Props = {
   user: any;
@@ -11,12 +12,14 @@ type Props = {
 function Sidebar({ user, setUser }: Props) {
   return (
     <div className='sidebar'>
+      <img src={dwLogo} alt='dev wiki logo' className='sidebar__logo' />
       <div className='sidebar__login'>
-        <h1>Dev Wiki</h1>
+        <h1 className='sidebar__title'>Dev Wiki</h1>
         <Login user={user} setUser={setUser} />
+      </div>
+      <div className='sidebar__wrapper'>
         <PageList user={user} />
       </div>
-      <div className='sidebar__wrapper'></div>
     </div>
   );
 }
