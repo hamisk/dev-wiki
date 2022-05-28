af866c40c5461b4e86f8c28bc7aa828c12fd2cd9
hamisk <hs.khera@icloud.com>
2022-05-28 12:33:13 -0700

    Initialise firebase database

--- client/src/firebase.ts
+++ client/src/firebase.ts
@@ -3,16 +3,18 @@ import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 import { getFirestore } from '@firebase/firestore';
 // https://firebase.google.com/docs/web/setup#available-libraries
+require('dotenv').config();
 
 // Your web app's Firebase configuration
+// Populate these variables from .env for security
 const firebaseConfig = {
-  apiKey: 'AIzaSyAJa2Z_n8UDddd3Sgk5UHQjT-ugZKHNfQk',
-  authDomain: 'dev-wiki-57987.firebaseapp.com',
-  databaseURL: 'https://dev-wiki-57987-default-rtdb.firebaseio.com',
-  projectId: 'dev-wiki-57987',
-  storageBucket: 'dev-wiki-57987.appspot.com',
-  messagingSenderId: '718213395294',
-  appId: '1:718213395294:web:b9642003c4055954fe95cf',
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
 };
 
 // Initialize Firebase
