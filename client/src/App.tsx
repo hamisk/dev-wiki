55a6d2174cfc1d81eedecfcdcb594d8b3d1e4f09
hamisk <hs.khera@icloud.com>
2022-05-28 17:13:30 -0700

    Connect to firebase database

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Page from './components/Page/Page';
 import Sidebar from './components/Sidebar/Sidebar';
+import { database } from './firebase-config';
+import { collection, getDocs } from 'firebase/firestore';
 
 function App() {
   const [user, setUser] = useState(null);
+  const [userlist, setUserlist] = useState([]);
+
+  const usersCollectionRef = collection(database, 'users');
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      console.log(data);
+    };
+    getUsers();
+  }, []);
 
   return (
     <div className='App'>
